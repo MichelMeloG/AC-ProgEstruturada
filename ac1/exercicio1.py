8ff8daa793c6071536b2ab196d812689c65e5ba9
MichelMeloG <161974251+MichelMeloG@users.noreply.github.com>
2024-03-02 17:30:45 -0300

    Incluindo código da AC1

---
+++ ac1/exercicio1.py
@@ -0,0 +1,11 @@
+a = int(input("qual o valor do parametro a da equação: "))
+b = int(input("qual o valor do parametro b da equação: "))
+c = int(input("qual o valor do parametro c da equação: "))
+
+delta = (b ** 2 - 4 * a * c)
+
+x1 = ((-b) + delta ** (1 / 2)) / (2 * a)
+x2 = ((-b) - delta ** (1 / 2)) / (2 * a)
+
+print(f'A primeira raiz da equação é: {x1}')
+print(f'A segunda raiz da equação é: {x2}')
